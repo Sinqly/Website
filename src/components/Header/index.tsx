549f04ce73970a6a0d052e2444c7e46cd80357b6
Bkuste2 <igorribeiro632@gmail.com>
2022-10-14 01:40:32 -0300

    feat(WIP): "RenderIndex" - Header Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,77 +1,103 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 
 import Logo from '../../assets/Images/Logo.png'
 import Input from '../Input'
+
 import {
-  Container,
-  ContainerLoginCadastro,
-  NavList,
+  IndexContainer,
+  MobileButton ,
   Nav,
+  Menu,
+  LogoSpace,
   ButtonLogin,
+  ButtonSpace,
+  ContainerLoginCadastro,
 } from './styles'
 
 interface HeaderProps {
   headerType?: string
 }
 
+const RenderIndex = () => {
+
+  const [active, setActive] = useState(false)
+
+  const toggleMenu = ()=>{
+
+    if(active === true) setActive(false)
+    else setActive(true)
+
+  }
+  
+  return (
+    <IndexContainer>
+      <Nav active={active}>
+        <MobileButton id={"botao-mobile"} onClick={toggleMenu}>
+          <span id={"hamburguer"}></span>
+        </MobileButton >
+
+        <Menu id={"menu"}>
+          <li>
+            <a href="./">Início</a>
+          </li>
+          <li>
+            <a href="#resume">Resumo</a>
+          </li>
+          <li>
+            <a href="#about">Sobre</a>
+          </li>
+          <li>
+            <a href="#areas">Áreas</a>
+          </li>
+        </Menu>
+      </Nav>
+      
+      <LogoSpace className={"logo-space"}>
+        <Image className={"logo"} src={Logo} alt="Logo" />
+      </LogoSpace>
+
+      <ButtonSpace className={"button-space"} >
+        <ButtonLogin onClick={() => console.log("OI")}>Login</ButtonLogin>
+      </ButtonSpace>
+
+    </IndexContainer>
+  )
+}
+
+const RenderSignInSignUp = () => {
+  return (
+    <IndexContainer>
+      <Input />
+      <Image style={{ width: '20%' }} src={Logo} alt="Logo" />
+      <ButtonLogin>voltar</ButtonLogin>
+    </IndexContainer>
+  )
+}
+
+const RenderGeneral = () => {
+  return (
+    <ContainerLoginCadastro>
+      <Input />
+      <Image
+        style={{ width: '20', justifySelf: 'center' }}
+        src={Logo}
+        alt="Logo"
+      />
+      <ButtonLogin>voltar</ButtonLogin>
+    </ContainerLoginCadastro>
+  )
+}
+
 const Header: React.FC<HeaderProps> = ({ headerType = 'General' }) => {
-  const Index = () => {
-    return (
-      <Container>
-        <Nav>
-          <NavList>
-            <li>
-              <a href="./">Início</a>
-            </li>
-            <li>
-              <a href="#resume">Resumo</a>
-            </li>
-            <li>
-              <a href="#about">Sobre</a>
-            </li>
-            <li>
-              <a href="#areas">Áreas</a>
-            </li>
-          </NavList>
-        </Nav>
-        <Image src={Logo} alt="Logo" />
-        <ButtonLogin>Login</ButtonLogin>
-      </Container>
-    )
+
+  const HeaderColection = {
+    Index: RenderIndex,
+    LoginCadastro: RenderSignInSignUp,
+    General: RenderGeneral,
   }
 
-  const LoginCadastro = () => {
-    return (
-      <Container>
-        <Input />
-        <Image style={{ width: '20%' }} src={Logo} alt="Logo" />
-        <ButtonLogin>voltar</ButtonLogin>
-      </Container>
-    )
-  }
-
-  const General = () => {
-    return (
-      <ContainerLoginCadastro>
-        <Input />
-        <Image
-          style={{ width: '20', justifySelf: 'center' }}
-          src={Logo}
-          alt="Logo"
-        />
-        <ButtonLogin>voltar</ButtonLogin>
-      </ContainerLoginCadastro>
-    )
-  }
-
-  const RenderHeader = {
-    Index,
-    LoginCadastro,
-    General,
-  }
-
-  return RenderHeader[headerType as keyof typeof RenderHeader]()
+  return HeaderColection[headerType as keyof typeof HeaderColection]()
 }
 
 export default Header
--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+export const IndexContainer = styled.div`
   width: 100%;
   height: 100px;
   padding: 32px 48px;
@@ -9,13 +9,112 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  @media screen and (max-width: 1024px){
+    #menu{
+      display: none;
+    }
+    
+    #botao-mobile {
+      display: block;
+    }
+
+    #menu{
+      display: block;
+      position: absolute;
+      width: 100vw;
+      top: 100px;
+      right: 0;
+      background-color: var(--white);
+      z-index: 1000;
+      height: 0px;
+      transition: .8s all;
+      visibility: hidden;
+      overflow-y: hidden;
+      border-top: 0.1px solid #d7d7d7;
+    }
+
+    #menu li {
+      padding: 1rem 0;
+      margin: 0 1rem;
+      border-bottom: 2px solid #6a6da62a;
+    }
+
+    #menu li a{
+        font-size: 20px;
+    }
+  }
+  @media screen and (max-width: 550px){
+    & {
+      padding: 16px 24px;
+    }
+    & > nav {
+      width: 30%;
+    }
+    .button-space{
+      display: none;
+    }
+    & .logo-space{
+      width: 70%;
+      justify-content: left;
+    }
+  }
+`
+
+export const MobileButton = styled.button`
+  border: none;
+  padding: .5rem 1rem;
+  font-size: 1rem;
+  background:none;
+  display: none;
+
+  #hamburguer{
+    display: block;
+    border-top: 2px solid;
+    width: 20px;
+    color: var(--light-purple);
+  }
+  
+  #hamburguer::after, #hamburguer::before{
+    content: '';
+    display: block;
+    border-top: 2px solid currentColor;
+    width: 20px;
+    background-color: currentColor;
+    margin-top: 5px;
+    transition: .3s;
+    position: relative;
+  }
+
+  & :hover{
+    cursor: pointer;
+  }
 `
 
 export const Nav = styled.nav`
-  width: 0;
+  width: 33%;
+  ${({ active }) => active ? `
+    #hamburguer{
+      border-top-color: transparent;
+    }
+  
+    #hamburguer::before{
+      transform: rotate(135deg);
+    }
+  
+    #hamburguer::after{
+      transform: rotate(-135deg);
+      top: -7px;
+    }   
+
+    #menu{
+      height: calc(100vh - 10vh);
+      visibility: visible;
+      overflow-y: auto;
+    }
+  `: ''}
 `
 
-export const NavList = styled.ul`
+export const Menu = styled.ul`
   display: flex;
   list-style: none;
   width: 100%;
@@ -23,14 +122,27 @@ export const NavList = styled.ul`
   & > li {
     margin-right: 16px;
   }
-
+  
   & > li > a {
     color: var(--dark-purple);
     text-decoration: none;
   }
 `
 
-export const ButtonLogin = styled.span`
+export const LogoSpace = styled.div`
+  width: 33%;
+  display:flex;
+  justify-content:center;
+  min-width: 150px;
+`
+
+export const ButtonSpace = styled.div`
+  width: 33%;
+  display:flex;
+  justify-content:right;
+`
+
+export const ButtonLogin = styled.button`
   width: 80px;
   height: 40px;
   display: flex;
@@ -40,6 +152,12 @@ export const ButtonLogin = styled.span`
   color: var(--white);
   background-color: var(--dark-purple);
   border-radius: 32px;
+  border: none;
+  
+  &:hover {
+    background-color: #3d1b74;
+    cursor: pointer;
+  }
 `
 
 export const ContainerLoginCadastro = styled.div`
