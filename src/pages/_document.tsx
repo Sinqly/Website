fca4a9a87cf419b1e3622139ec9beeecfc9e8f51
ismaelsilvat <ismaelteixeiradas1@hotmail.com>
2022-10-17 19:17:22 -0300

    feat:Updating Font Link Import

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -43,6 +43,16 @@ export default class MyDocument extends Document {
       <Html style={{ scrollBehavior: 'smooth' }} lang="pt">
         <Head>
           <meta charSet="utf-8" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
-
   * {
     margin: 0;
     padding: 0;
