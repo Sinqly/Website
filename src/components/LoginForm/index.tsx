3f9d0223cdb6a2bd1ded5fdbc10593b04e4a8686
IGOR MEDEIROS RIBEIRO <igor_m_ribeiro@estudante.sc.senai.br>
2022-11-03 12:38:22 -0300

    feat(WIP): loginForm - Component

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -2,29 +2,29 @@
 import { Formik } from 'formik';
 import React from 'react';
 import * as yup from 'yup';
-import { Button, ButtonDiv, EmailIcon, Input,  InputField, InputGroup, StayConnected, PasswordIcon } from './styles';
+import { Button, ButtonDiv, EmailIcon, Input, InputField, InputGroup, StayConnected, PasswordIcon } from './styles';
 
-interface formProps {}
+interface formProps { }
 
 const LoginForm: React.FC<formProps> = () => {
-  
-  const yupValidationSchema = yup.object().shape({
-    
-    email: 
-      yup.string()
-      .email("Email inválido!")
-      .required("Email Obrigatório"),
 
-    password: 
+  const yupValidationSchema = yup.object().shape({
+
+    email:
       yup.string()
-      .min(8, "A senha deve possuir pelo menos 8 caracteres")
-      .required("Senha obrigatória!"),
+        .email("Email inválido!")
+        .required("Email Obrigatório"),
+
+    password:
+      yup.string()
+        .min(6, "A senha deve possuir pelo menos 6 caracteres")
+        .required("Senha obrigatória!"),
 
   });
 
-  const initialValues = { 
-    email: "", 
-    password: "" 
+  const initialValues = {
+    email: "",
+    password: ""
   }
 
   const handleSubmitForm = ({ email, password }): void => {
@@ -36,20 +36,20 @@ const LoginForm: React.FC<formProps> = () => {
   }
 
   return (
-    <Formik 
-      validateOnBlur={ false } 
-      validateOnChange={ false } 
-      onSubmit={ handleSubmitForm } 
-      initialValues={ initialValues } 
-      validationSchema={ yupValidationSchema }
+    <Formik
+      validateOnBlur={false}
+      validateOnChange={false}
+      onSubmit={handleSubmitForm}
+      initialValues={initialValues}
+      validationSchema={yupValidationSchema}
     >
       { // Chave para manipular tsx
         ({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => (
-          <form onSubmit={handleSubmit} style={{width: '100%', maxWidth: 557}}>
+          <form onSubmit={handleSubmit} style={{ width: '100%', maxWidth: 557 }}>
             <InputGroup>
               <div>
                 <InputField>
-                  <EmailIcon onClick={() => handleFocusInput('inputEmail')}/>
+                  <EmailIcon onClick={() => handleFocusInput('inputEmail')} />
                   <Input
                     type="text"
                     name="email"
@@ -64,7 +64,7 @@ const LoginForm: React.FC<formProps> = () => {
               </div>
               <div>
                 <InputField>
-                  <PasswordIcon onClick={() => handleFocusInput('inputPassword')}/>
+                  <PasswordIcon onClick={() => handleFocusInput('inputPassword')} />
                   <Input
                     type="password"
                     name="password"
@@ -84,7 +84,7 @@ const LoginForm: React.FC<formProps> = () => {
                 <label htmlFor='stayConnected'>Deseja continuar conectado?</label>
               </StayConnected>
             </InputGroup>
-                  
+
             <ButtonDiv>
               <Button type="submit">Logar</Button>
             </ButtonDiv>
