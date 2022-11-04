66219c257eb9e8de6819b425da55c17a900466ef
ismaelsilvat <ismaelteixeiradas1@hotmail.com>
2022-11-04 19:01:12 -0300

    feat: Adding Footer Svg's

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react'
 
+import Svg1 from '../assets/Images/Index/leaf-circle.svg'
+import Svg2 from '../assets/Images/Index/web-circle.svg'
+import Svg3 from '../assets/Images/Index/monument-circle.svg'
+import Svg4 from '../assets/Images/Index/math-circle.svg'
+import Svg5 from '../assets/Images/Index/laptop-circle.svg'
+import Svg6 from '../assets/Images/Index/carrot-circle.svg'
+
+
 interface FooterProps {
   footerType?: string
 }
