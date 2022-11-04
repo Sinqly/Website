a68a032b3582f6d58393a205db80d5679d2cf92d
ismaelsilvat <ismaelteixeiradas1@hotmail.com>
2022-11-04 11:41:03 -0300

    feat: Adding Footer Folder

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+interface headerProps { }
+
+const Header: React.FC<headerProps> = props => {
+    return (
+        <h1>Header</h1>
+    )
+};
+
+export default Header;
