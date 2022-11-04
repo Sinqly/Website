588f71ada925ca2aa9d84a3b49472100c7553382
ismaelsilvat <ismaelteixeiradas1@hotmail.com>
2022-11-04 19:08:20 -0300

    feat: Adding Initial Index Footer Model

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -14,14 +14,51 @@ interface FooterProps {
 const Footer: React.FC<FooterProps> = ({ footerType }) => {
   const Index = () => {
     return (
-      <>
-        <Svg1 />
-        <Svg2 />
-        <Svg3 />
-        <Svg4 />
-        <Svg5 />
-        <Svg6 />
-      </>
+      <footer>
+        <div>
+          <h2>Sinqly</h2>
+          <p>
+            Sinqly is a system who gives to the students a platform to publish
+            they projects. This platform make possibly people connect and
+            interact each other.
+          </p>
+        </div>
+        <div>
+          <div>
+            <h4>Início</h4>
+            <p>
+              Aqui falamos de maneira mais completa sobre o objetivo da Sinqly
+            </p>
+          </div>
+          <div>
+            <h4>Áreas de conhecimento</h4>
+            <div>
+              <Svg1 />
+              <Svg2 />
+              <Svg3 />
+              <Svg4 />
+              <Svg5 />
+              <Svg6 />
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h4>Resumo</h4>
+            <p>
+              Aqui falamos resumidamente sobre o objetivo da Sinqly com uma
+              imagem contextualizada de fundo
+            </p>
+          </div>
+          <div>
+            <h4>Sobre</h4>
+            <p>
+              Aqui falamos de maneira mais completa sobre o objetivo da Sinqly
+            </p>
+          </div>
+        </div>
+        <h5>©Todos os direitos reservados por Sinqly 2022</h5>
+      </footer>
     )
   }
   const Feed = () => {
