c21c0361236d995573ecd117622cee82807b5300
ismaelsilvat <ismaelteixeiradas1@hotmail.com>
2022-11-04 19:02:43 -0300

    feat: Solving Import Error

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,26 +1,34 @@
 import * as React from 'react'
 
-import Svg1 from '../assets/Images/Index/leaf-circle.svg'
-import Svg2 from '../assets/Images/Index/web-circle.svg'
-import Svg3 from '../assets/Images/Index/monument-circle.svg'
-import Svg4 from '../assets/Images/Index/math-circle.svg'
-import Svg5 from '../assets/Images/Index/laptop-circle.svg'
-import Svg6 from '../assets/Images/Index/carrot-circle.svg'
-
+import Svg1 from '../../assets/Images/Index/leaf-circle.svg'
+import Svg2 from '../../assets/Images/Index/web-circle.svg'
+import Svg3 from '../../assets/Images/Index/monument-circle.svg'
+import Svg4 from '../../assets/Images/Index/math-circle.svg'
+import Svg5 from '../../assets/Images/Index/laptop-circle.svg'
+import Svg6 from '../../assets/Images/Index/carrot-circle.svg'
 
 interface FooterProps {
   footerType?: string
 }
 
-const Footer: React.FC<FooterProps> = ({footerType}) => {
+const Footer: React.FC<FooterProps> = ({ footerType }) => {
   const Index = () => {
-    return <h1>Sou o Index</h1>
+    return (
+      <>
+        <Svg1 />
+        <Svg2 />
+        <Svg3 />
+        <Svg4 />
+        <Svg5 />
+        <Svg6 />
+      </>
+    )
   }
   const Feed = () => {
     return <h1>Sou o Feed</h1>
   }
 
-  const FooterColection = { Index, Feed}
+  const FooterColection = { Index, Feed }
 
   return FooterColection[footerType as keyof typeof FooterColection]()
 }
