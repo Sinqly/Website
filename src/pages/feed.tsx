b17d368ce4c5a3d6872648652c6207619c1a3a57
Bkuste2 <igorribeiro632@gmail.com>
2022-11-18 08:20:29 -0300

    feat(WIP): Ending Card Component  - Feed Page

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -1,21 +1,76 @@
+import Image from 'next/image';
 import * as React from 'react';
+
+import Careca from '../assets/Images/Feed/Careca.png'
+import Imagem from '../assets/Images/Feed/imagem_feed.jpg'
 import Header from '../components/Header';
-import { Container, SideCard, Content, UserList, } from '../styles/pages/feed';
+import { Container, SideCard, Content, UserList, Card, CardHeader, UserImage, Texts, Nome, Username, CardBody, Actions, Like, Comments, Save, NotLike } from '../styles/pages/feed';
 
-interface feedProps {}
+// interface feedProps {}
 
-const feed: React.FC<feedProps> = props => {
+const feed: React.FC = () => {
   return (
     <>
       <Header headerType='General' />
       <Container>
         <SideCard>
-          
+          Side Card
         </SideCard>
         <Content>
-
+          <Card>
+            <CardHeader>
+              <UserImage>
+                <Image  src={Careca} alt="Foto do usuário" />
+              </UserImage>
+              <Texts>
+                <Nome href='https://www.google.com'>Paulo Battistella</Nome>
+                <Username>@pbattistella#2021</Username>
+              </Texts>
+            </CardHeader>
+            <CardBody>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem fuga doloremque quis, obcaecati libero tenetur delectus assumenda sapiente amet, incidunt temporibus perspiciatis iste eligendi aperiam ex magnam voluptatum, iure aut? Cum consectetur similique accusantium aliquid eveniet, quo a at est sapiente nulla fuga, totam nam, illum deserunt molestias dolor ullam quam. Provident! Cum consectetur similique accusantium aliquid eveniet, quo a at est sapiente nulla fuga, totam nam, illum deserunt molestias dolor ullam quam. Provident! Cum consectetur similique accusantium aliquid eveniet, quo a at est sapiente nulla fuga, totam nam, illum deserunt molestias dolor ullam quam. Provident!
+              </p>
+              <Image src={Imagem} width="1000" alt='Oi'/>
+              <Actions>
+                <div>
+                  <Like/>
+                  <Comments/>
+                </div>
+                <Save />
+              </Actions>
+            </CardBody>
+          </Card>
+          <Card>
+            <CardHeader>
+              <UserImage>
+                <Image  src={Careca} alt="Foto do usuário" />
+              </UserImage>
+              <Texts>
+                <Nome href='https://www.google.com'>Paulo Battistella</Nome>
+                <Username>@pbattistella#2021</Username>
+              </Texts>
+            </CardHeader>
+            <CardBody>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem fuga doloremque quis, obcaecati libero tenetur delectus assumenda sapiente amet, incidunt temporibus perspiciatis iste eligendi aperiam ex magnam voluptatum, iure aut? Cum consectetur similique accusantium aliquid eveniet, quo a at est sapiente nulla fuga, totam nam, illum deserunt molestias dolor ullam quam. Provident! Cum consectetur similique accusantium aliquid eveniet, quo a at est sapiente nulla fuga, totam nam, illum deserunt molestias dolor ullam quam. Provident! Cum consectetur similique accusantium aliquid eveniet, quo a at est sapiente nulla fuga, totam nam, illum deserunt molestias dolor ullam quam. Provident!
+              </p>
+              <Image src={Imagem} width="1000" alt='Oi'/>
+              <Actions>
+                <div>
+                  <NotLike />
+                  <Comments/>
+                </div>
+                <Save/>
+              </Actions>
+            </CardBody>
+          </Card>
         </Content>
+
         <UserList>
+          <h2>Online Connections</h2>
+            
+          <h2>Offline Connections</h2>
 
         </UserList>
       </Container>
--- src/styles/pages/feed.tsx
+++ src/styles/pages/feed.tsx
@@ -1,21 +1,127 @@
 import styled from 'styled-components'
 
+import { FcLike, FcLikePlaceholder } from 'react-icons/fc'
+import { FaRegComment, FaRegFlag } from 'react-icons/fa'
+
+
 export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 430px auto 284px;
+  grid-template-rows: 568px auto;
+  grid-template-areas: 
+    'SideCard Content UserList'
+    'Empty    Content UserList '
+  ;
   width: 100vw;
+  height: calc(100%);
 `
 
 export const SideCard = styled.div`
-  width: 20%;
-  min-width: 300px;
+  grid-area: SideCard;
   background-color: red;
-  min-height: 350px;
-  position: absolute;
-  top: 26%;
-  left: 5%;
+  position: fixed;
+  top: 30%;
+  left: 2%;
+
+  width: 350px;
+  height: 430px;
 ` 
 export const Content = styled.div`
-  
+  grid-area: Content;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
 ` 
 export const UserList = styled.div`
-  
+  grid-area: UserList;
+  position: sticky;
+  box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.35);
+  -webkit-box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.35);
+  -moz-box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.35);
+`
+
+export const EMPTY = styled.div`
+  grid-area: Empty;
+`
+
+export const Card = styled.div`
+  width: 70%;
+  margin-top: 30px;
+  padding: 25px;
+  min-width: 550px;
+`
+
+export const CardHeader = styled.header`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+`
+
+export const UserImage = styled.div`
+  width: 40%;
+  max-width: 80px;
+  max-height: 80px;
+  border-radius: 100%;
+
+  img{
+    border-radius: 100%;
+  }
+`
+
+export const Texts = styled.div`
+  margin-left: 30px;
+`
+
+export const Nome = styled.a`
+  color: var(--black);
+  font-weight: bold;
+`
+
+export const Username = styled.div`
+  color: var(--grey);
+`
+
+export const CardBody = styled.div`
+  margin-top: 3%;
+
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  text-align: justify;
+`
+
+export const Actions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+
+  padding: 0 2%;
+` 
+
+
+
+export const NotLike = styled(FcLikePlaceholder)`
+  width: 36px;
+  height: 36px;
+`
+
+export const Like = styled(FcLike)`
+  width: 36px;
+  height: 36px;
+` 
+
+export const Comments = styled(FaRegComment)`
+  width: 30px;
+  height: 30px;
+  margin-left: 16px;
+`
+
+export const Save = styled(FaRegFlag)`
+  width: 30px;
+  height: 30px;
+  margin-left: 16px;
 `
