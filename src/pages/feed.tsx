c7f14dda40f921e686dda9f6ab9956a3f113a8f2
bkuste2 <igorribeiro632@gmail.com>
2022-11-24 15:11:38 -0300

    update: some updates on Conde

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -6,24 +6,38 @@ import { SideCardHome } from '../components/SideCard'
 import UserList from '../components/UserList'
 import { api } from '../config/Axios'
 
-import { Container, Content } from '../styles/pages/feed'
+import { Container, Content, ButtonModalCreate } from '../styles/pages/feed'
 
 import { PostCardInterface } from '../utils/feed/PostCardInterface'
 
 const Feed: React.FC = () => {
   const [posts, setPosts] = useState<PostCardInterface[]>([])
+  const [active, setActive] = useState<boolean>(false)
 
+
+  const toggleMenu = () => {
+    console.log(active);
+    if (active === true) setActive(false)
+    else setActive(true)
+  }
   
   useEffect(() => {
     api.get('/posts')
-      .then(posts => setPosts(posts.data))
+      .then(posts => setPosts(posts.data))      
   }, [])
+
   return (
     <>
+      {
+        active ? <div style={{position: "absolute", width: "100vw", height: "calc(100vh)", backgroundColor: "red", zIndex: 800}}> buceta preta</div> : null
+      }
       <Header headerType="General" />
       <Container>
         <SideCardHome />
         <Content className="content">
+          <ButtonModalCreate onClick={toggleMenu}>
+            Cria Novo Post
+          </ButtonModalCreate>
           { 
            posts.map(post => {              
               return <PostCard 
